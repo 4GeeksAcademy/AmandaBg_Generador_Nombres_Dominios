bd43fad2d22d0f5778cbd91a4ee5f1be051f4bf4
abarrelier <128666422+abarrelier@users.noreply.github.com>
2023-06-20 02:45:38 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -9,3 +9,19 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let expresions = [".com", ".us", ".io"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let m = 0; m < expresions.length; m++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + expresions[m]);
+      }
+    }
+  }
+}
+
